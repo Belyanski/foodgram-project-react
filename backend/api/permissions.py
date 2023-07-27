5cb6513539f0db441ba51e88e6b788d70e75a54b
Belyanski <yaroslav.belyanski@yandex.ru>
2023-07-27 13:28:45 +0300

    Fix 291

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,7 +5,7 @@ class IsAuthorAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.user.is_authenticated and (
-                request.user.is_superuser or obj.author == request.user 
+                request.user.is_superuser or obj.author == request.user
                 or request.method == 'POST'
             )
         ) or request.method in permissions.SAFE_METHODS
