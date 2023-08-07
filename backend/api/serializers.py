bf2f903f98d98cf0c7df459b512c8482cab7af79
Belyanski <yaroslav.belyanski@yandex.ru>
2023-08-07 03:21:49 +0300

    Fix fix fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,7 +9,7 @@ from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
 from rest_framework.exceptions import ValidationError
 from rest_framework.fields import IntegerField, SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import CharField, ModelSerializer
 from users.models import Subscribe, User
 
 
